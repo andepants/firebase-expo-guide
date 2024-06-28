91bc2edf2170d7688a9079de5797ef7b7a605867
Andrew Heim <andrewsheim@gmail.com>
2024-06-28 16:40:05 -0700

    Initial Firebase Setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -51,6 +51,8 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
